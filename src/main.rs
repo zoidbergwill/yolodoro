ef1359c8803027070058ff0b5842d5516f6150cd
William Stewart <zoidbergwill@gmail.com>
2019-02-20 17:32:14 +0100

    Add ALL the dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+extern crate ctrlc;
 extern crate notify_rust;
+extern crate quicli;
+extern crate structopt;
+
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
 
 use notify_rust::Notification;
 use notify_rust::NotificationHint as Hint;
-
-use std::time::Duration;
-use std::thread;
-
-const POMODORO_SECONDS: u64 = 60*24;
-// const POMODORO_SECONDS: u64 = 3;
-const SHORT_PAUSE_SECONDS: u64 = 60*5;
-const LONG_PAUSE_SECONDS: u64 = 60*20;
+use quicli::prelude::*;
+use structopt::StructOpt;
 
 fn blocking_notification(summary: &str, body: &str) {
   #[allow(unused_variables)]
@@ -26,32 +28,65 @@ fn blocking_notification(summary: &str, body: &str) {
   });
 }
 
-fn main() {
-  let pomodoro = Duration::new(POMODORO_SECONDS, 0);
-  let short_pause = Duration::new(SHORT_PAUSE_SECONDS, 0);
-  let long_pause = Duration::new(LONG_PAUSE_SECONDS, 0);
+/// Read some lines of a file
+#[derive(Debug, StructOpt)]
+#[structopt(
+  name = "yolodoro",
+  about = "Simplest Pomodoro timer you can think of, written in Rust."
+)]
+struct Cli {
+  /// Number of lines to read
+  #[structopt(short = "n")]
+  num: u64,
+  /// Length of pomodoro
+  #[structopt(short = "l", default_value = "24")]
+  length: u64,
+  /// Length of short pause
+  #[structopt(short = "s", default_value = "5")]
+  short_pause: u64,
+  /// Length of long pause
+  #[structopt(short = "lp", default_value = "20")]
+  long_pause: u64,
+}
+
+fn main() -> CliResult {
+  let args = Cli::from_args();
+  let pomodoro = Duration::new(args.length * 60, 0);
+  let short_pause = Duration::new(args.short_pause * 60, 0);
+  let long_pause = Duration::new(args.long_pause * 60, 0);
 
   let mut count = 0;
-  loop {
-    let pomodoro_msg = format!("Starting new pomodoro: {}m", (POMODORO_SECONDS / 60));
-    blocking_notification(&pomodoro_msg.to_owned(), "");
-    println!("{}", pomodoro_msg);
-    thread::sleep(pomodoro);
+  let running = Arc::new(AtomicBool::new(true));
+  let r = running.clone();
+  ctrlc::set_handler(move || {
+    r.store(false, Ordering::SeqCst);
+  })
+  .expect("Error setting Ctrl-C handler");
+  println!("Waiting for Ctrl-C...");
+  while running.load(Ordering::SeqCst) {
+    thread::spawn(move || loop {
+      let pomodoro_msg = format!("Starting new pomodoro: {}m", (pomodoro.as_secs() / 60));
+      blocking_notification(&pomodoro_msg.to_owned(), "");
+      println!("{}", pomodoro_msg);
+      thread::sleep(pomodoro);
 
-    println!("Pomodoro done");
-    count += 1;
+      println!("Pomodoro done");
+      count += 1;
 
-    let duration = if count % 4 == 0 {
-      let pause_msg = format!("Ready for a long pause: {}m", (LONG_PAUSE_SECONDS / 60));
-      println!("{}", pause_msg);
-      blocking_notification(&pause_msg.to_owned(), "");
-      long_pause
-    } else {
-      let pause_msg = format!("Ready for a short pause: {}m", (SHORT_PAUSE_SECONDS / 60));
-      println!("{}", pause_msg);
-      blocking_notification(&pause_msg.to_owned(), "");
-      short_pause
-    };
-    thread::sleep(duration);
+      let duration = if count % 4 == 0 {
+        let pause_msg = format!("Ready for a long pause: {}m", (long_pause.as_secs() / 60));
+        println!("{}", pause_msg);
+        blocking_notification(&pause_msg.to_owned(), "");
+        long_pause
+      } else {
+        let pause_msg = format!("Ready for a short pause: {}m", (short_pause.as_secs() / 60));
+        println!("{}", pause_msg);
+        blocking_notification(&pause_msg.to_owned(), "");
+        short_pause
+      };
+      thread::sleep(duration);
+    });
   }
+  println!("Got it! Exiting...");
+  Ok(())
 }
