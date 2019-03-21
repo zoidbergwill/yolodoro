cc97fcf14cb09e46fee641b6c151ce5b1f98381a
William Stewart <zoidbergwill@gmail.com>
2019-03-21 14:10:55 +0100

    Make it post notifications, but only work for MacOS
    Using great code from https://github.com/frewsxcv/alert-after

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,45 @@
 extern crate ctrlc;
-extern crate notify_rust;
 extern crate quicli;
 extern crate structopt;
 
-use std::io;
-use std::io::Write;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
-use notify_rust::Notification;
-use notify_rust::NotificationHint as Hint;
 use quicli::prelude::*;
 use structopt::StructOpt;
 
-fn blocking_notification(summary: &str, body: &str) {
-    #[allow(unused_variables)]
-    let notification_handle = Notification::new()
-        .summary(summary)
-        .body(body)
-        .hint(Hint::Resident(true))
-        .show()
-        .unwrap();
+#[cfg(target_os = "macos")]
+extern crate mac_notification_sys;
 
-    #[cfg(all(unix, not(target_os = "macos")))]
-    notification_handle.wait_for_action(|_action| {
-        println!("Notification was closed");
-    });
+
+#[cfg(target_os = "macos")]
+struct MacOs;
+
+trait Platform {
+    fn setup() -> Self;
+    fn notify(msg_title: &str, msg_body: &str);
+    fn teardown(&mut self);
 }
+
+#[cfg(target_os = "macos")]
+impl Platform for MacOs {
+    fn setup() -> Self {
+        MacOs
+    }
+
+    fn notify(msg_title: &str, msg_body: &str) {
+        let bundle = mac_notification_sys::get_bundle_identifier("Script Editor").unwrap();
+        mac_notification_sys::set_application(&bundle).unwrap();
+        mac_notification_sys::send_notification(msg_title, &None, msg_body, &None).unwrap();
+    }
+
+    fn teardown(&mut self) {}
+}
+
+#[cfg(target_os = "macos")]
+type CurrPlatform = MacOs;
 
 /// Read some lines of a file
 #[derive(Debug, StructOpt)]
@@ -38,17 +49,18 @@ fn blocking_notification(summary: &str, body: &str) {
 )]
 struct Cli {
     /// Length of pomodoro
-    #[structopt(short = "l", default_value = "24")]
+    #[structopt(default_value = "24")]
     length: u64,
     /// Length of short pause
-    #[structopt(short = "s", default_value = "5")]
+    #[structopt(default_value = "5")]
     short_pause: u64,
     /// Length of long pause
-    #[structopt(short = "lp", default_value = "20")]
+    #[structopt(default_value = "20")]
     long_pause: u64,
 }
 
 fn main() -> CliResult {
+    let mut p = CurrPlatform::setup();
     let args = Cli::from_args();
     let pomodoro = Duration::new(args.length * 60, 0);
     let short_pause = Duration::new(args.short_pause * 60, 0);
@@ -64,9 +76,9 @@ fn main() -> CliResult {
     println!("Waiting for Ctrl-C...");
     thread::spawn(move || loop {
         let pomodoro_msg = format!("Starting new pomodoro: {}m", (pomodoro.as_secs() / 60));
-        let stdout = io::stdout();
-        let _ = writeln!(&mut stdout.lock(), "{}", pomodoro_msg);
-        blocking_notification(&pomodoro_msg.to_owned(), "");
+        println!("{}", pomodoro_msg);
+		CurrPlatform::notify("Yolodoro", &pomodoro_msg);
+        println!("Thread is going to sleep");
         thread::sleep(pomodoro);
 
         println!("Pomodoro done");
@@ -74,18 +86,20 @@ fn main() -> CliResult {
 
         let duration = if count % 4 == 0 {
             let pause_msg = format!("Ready for a long pause: {}m", (long_pause.as_secs() / 60));
-            let _ = writeln!(&mut stdout.lock(), "{}", pause_msg);
-            blocking_notification(&pause_msg.to_owned(), &pause_msg);
+            println!("{}", pause_msg);
+			CurrPlatform::notify("Yolodoro", &pause_msg);
             long_pause
         } else {
             let pause_msg = format!("Ready for a short pause: {}m", (short_pause.as_secs() / 60));
-            let _ = writeln!(&mut stdout.lock(), "{}", pause_msg);
-            blocking_notification(&pause_msg.to_owned(), &pause_msg);
+            println!("{}", pause_msg);
+			CurrPlatform::notify("Yolodoro", &pause_msg);
             short_pause
         };
+        println!("Thread is going to sleep");
         thread::sleep(duration);
     });
     while running.load(Ordering::SeqCst) {}
     println!("Got it! Exiting...");
+    p.teardown();
     Ok(())
 }
